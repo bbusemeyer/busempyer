d3ad9d46f1dd23fa535ded28797d8f43e2df96f6
= <=>
2015-11-03 14:45:31 -0600

    modified:   drivers/run_ppr.py 	modified:   drivers/scan_pw.py

--- drivers/scan_pw.py
+++ drivers/scan_pw.py
@@ -40,9 +40,10 @@ with open("base.inp",'w') as outf:
 cwd = os.getcwd()
 
 changes = {
-  "kpoint":[2,4,6,8,10],
-  "ecutwfc":[50,60,70,80,90,100],
-  "ecutrho":[400]
+    "lambda":[0.01,0.02,0.03,0.1,0.2]
+  #"kpoint":[2,4,6,8,10],
+  #"ecutwfc":[50,60,70,80,90,100],
+  #"ecutrho":[400]
 }
 
 # Now's the part where you mess with something.
