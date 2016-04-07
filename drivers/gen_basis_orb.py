b5325589854c60af4f0c9407b7df90c76d725170
Brian Busemeyer <bbusemeyer@gmail.com>
2016-04-07 14:42:49 -0500

    Better automatic gen_basis_orb

--- drivers/gen_basis_orb.py
+++ drivers/gen_basis_orb.py
@@ -4,34 +4,54 @@
 # is simply the corresponding basis element.
 import sys
 
-print("You'll need to input the element names in order, followed by the number present in the material.")
-done = False
-basis_numbers = {}
-while not done:
-  ion_name = input("Element name (empty for done):")
-  if ion_name != "":
-    ion_number = int(input("Number of %s ions:"%ion_name))
-    basis_numbers[ion_name] = range(1,ion_number+1)
-  else:
-    done = True
+if len(sys.argv) != 4:
+  print("Usage: ")
+  print("python gen_basis_orb.py your.sys your.min.basis output.orb")
 
-# If you're reading this because you're annoyed at using the interface, go
-# ahead and hard-code the dict here:
-#basis_numbers={"FE":range(1,11),"SE":range(1,5)}
+sysf=open(sys.argv[1],'r')
+basfn=sys.argv[2]
+outf=open(sys.argv[3],'w')
 
-f=open(sys.argv[1],'r')
+# Convert basis type to range of basis elements numbers.
+basis_converter = {"S":1,"P":3,"5D":5}
+def find_basis_numbers(element,basfn):
+  basf = open(basfn,'r')
+  num_basis = 0
+  baswords = basf.read().split()
+  elmflg = False
+  basflg = False
+  for wnum,word in enumerate(baswords):
+    if word == element: elmflg = True
+    if (word == "{") and (elmflg): basflg = True
+    if elmflg and basflg and (word in basis_converter.keys()):
+      num_basis+=basis_converter[word]
+    if elmflg and basflg and word == "}":
+      basf.close()
+      return range(1,num_basis+1)
+  basf.close()
+  return "Error in find_num_basis!"
 
-totmonum=1
-atomnum=1
-for line in f:
+
+# Collect list of atoms from sys file.
+atomlist = []
+for line in sysf:
   #assume that the ATOM section is all on one line
   #as is printed out by the converters
   if "ATOM" in line:
     spl=line.split()
-    nm=spl[2]
-    for b in basis_numbers[nm]:
-      print "%i %i %i 1"%(totmonum,b,atomnum)
-      totmonum+=1
-    atomnum+=1
-print "COEFFICIENTS\n1.0\n\n"
+    atomlist.append(spl[2])
 
+# Find number of basis elements for each atom.
+basis_numbers = {}
+for atom in set(atomlist):
+  basis_numbers[atom] = find_basis_numbers(atom,basfn)
+
+# Output orb file.
+totmonum=1
+atomnum=1
+for atom in atomlist:
+  for b in basis_numbers[atom]:
+    outf.write("%i %i %i 1\n"%(totmonum,b,atomnum))
+    totmonum+=1
+  atomnum+=1
+outf.write("COEFFICIENTS\n1.0\n")
