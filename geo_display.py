878b53ffa932430f45627f973d31eba89c8f9772
Brian Busemeyer <bbusemeyer@gmail.com>
2018-01-17 12:04:06 -0600

    Updated for py3

--- geo_display.py
+++ geo_display.py
@@ -14,18 +14,18 @@ def readgeo(inpf):
   while cur != []:
     cur.pop(3)
     cur.pop(1)
-    dat.append(map(float,cur))
+    dat.append(cur)
     cur = inpf.readline().split()
 
-  return array(dat).T
+  return array(dat).astype(float).T
 
 
 atis, atyps, ax, ay, az = readgeo(open(sys.argv[1],'r'))
 ax%=1; ay%=1; az%=1
 
 nfe = list(atyps).count(226)
-points = zip(ax[0:nfe],ay[0:nfe])
-slabs  = map(int,atis[0:nfe])
+points = list(zip(ax[0:nfe],ay[0:nfe]))
+slabs  = atis[0:nfe].astype(int)
 
 plot(ax[0:nfe],ay[0:nfe],'k.')
 title(sys.argv[1])
