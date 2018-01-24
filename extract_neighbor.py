6131c5843bbc8293bb1fd86bf2a04b66e78aa2d4
Brian Busemeyer <bbusemeyer@gmail.com>
2018-01-24 10:59:04 -0600

    python 3 	modified:   extract_neighbor.py

--- extract_neighbor.py
+++ extract_neighbor.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 from numpy import *
 
+def safeint(thing):
+  try: return int(thing)
+  except ValueError: return thing
+
 def read_map(inpf):
   line = "Start"
   found_spot = False
@@ -15,15 +19,16 @@ def read_map(inpf):
     if "SYMMETRY" in line: break
       
     anum, elem, nnbr, rnbr, dumy = line.split()[:5]
-    anum, nnbr = map(int,[anum,nnbr])
-    rnbr = float(rnbr)
+    anum=int(anum)
+    nnbr=int(nnbr)
+
     tmp = line[33:-1]
     for nlines in range(nnbr//3 - 1):
       tmp += inpf.readline()[:-1]
     if (nnbr > 3) and (nnbr % 3 != 0):
       tmp += inpf.readline()[:-1]
     tmp = tmp.replace('-',' -').split()
-    nbrs = map(int,[tmp[5*i:5*(i+1)] + [rnbr] for i in range(nnbr)])
+    nbrs = [[safeint(t) for t in tmp[5*i:5*(i+1)]] + [float(rnbr)] for i in range(nnbr)]
 
     if anum in nnmap.keys():
       nnmap[anum] += nbrs
@@ -41,14 +46,13 @@ def map_to_table(nnmap):
 if __name__ == "__main__":
   import sys
   inpf = open(sys.argv[1],'r')
-  outf = open("neighbor_table.csv",'w')
+  #outf = open("neighbor_table.csv",'w')
   
   nnmap = read_map(inpf)
+  print(nnmap)
 
-  # Output data to usable table.
-  outf.write("anum,nnum,nelm,cx,cy,cz,nrad\n")
-  for key in nnmap.keys():
-    for neighbors in nnmap[key]:
-      outf.write('%d,%s,%s,%s,%s,%s,%s\n'%tuple([key]+neighbors))
-  inpf.close()
-  outf.close()
+  ## Output data to usable table.
+  #print("anum  nnum  nelm  cx  cy  cz  nrad\n")
+  #for key in nnmap.keys():
+  #  for neighbors in nnmap[key]:
+  #    print('%s  %s  %s  %s  %s  %s  %s\n'%tuple([key]+neighbors))
