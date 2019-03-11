5ab45ecda1c76ac1867900ade675974fd70939c8
Brian Busemeyer <bbusemeyer@gmail.com>
2019-03-11 18:27:52 -0500

    Simplified orbital ploting function using qwalk_objects.

--- busempyer/getorbs.py
+++ busempyer/getorbs.py
@@ -1,86 +1,22 @@
 # Brian Busemeyer
 # Extract up and down orbitals, write plot method for qwalk
+from pickle import load
 
-def produce_inputs(fileroot):
-  openf = open(fileroot+'.slater','r')
-  upoutpf = open('orbplotup','w')
-  dnoutpf = open('orbplotdn','w')
-  uporbs = []
-  dnorbs = []
+def main():
+  from_conv('conv.pkl','crys_0.orb',range(1,45))
 
-  upoutpf.write('method { PLOT\n')
-  dnoutpf.write('method { PLOT\n')
-  upoutpf.write('  resolution 0.1 \n')
-  dnoutpf.write('  resolution 0.1 \n')
+def from_conv(convmgr,orbfile,orbitals):
+  conv = load(open(convmgr,'rb'))
+  orblines = conv.orbitals[0].export_qwalk_orbitals(orbfile).split('\n')
+  syslines = conv.system.export_qwalk_sys().split('\n')
 
-  ln = openf.readline().split()[0]
+  outlines = [
+      'method { plot',
+      '  resolution 0.1',
+      '  plotorbitals { ' + ' '.join(map(str,orbitals)) + '}',
+    ] + ['  ' + line for line in orblines] + syslines
 
-  while ln.split()[0] != '}': 
-    ln = openf.readline()
-    upoutpf.write(ln)
-    dnoutpf.write(ln)
-
-  while (ln != ['#Spin','up','orbitals']) and (ln != ['#','Spin','up','orbitals.']): 
-    ln = openf.readline().split()
-      
-  ln = openf.readline().split()
-  while (ln != ['#Spin','down','orbitals']) and (ln != ['#','Spin','down','orbitals.']): 
-    uporbs += ln
-    ln = openf.readline().split()
-
-  ln = openf.readline().split()
-  while ln != []:
-    if ln[-1] == '}': del ln[-1]
-    dnorbs += ln
-    ln = openf.readline().split()
-
-  upoutpf.write('PLOTORBITALS{ ')
-  dnoutpf.write('PLOTORBITALS{ ')
-  for oi in uporbs: upoutpf.write(oi+' ')
-  for oi in dnorbs: dnoutpf.write(oi+' ')
-  upoutpf.write('}')
-  dnoutpf.write('}')
-  upoutpf.write('\n}\n')
-  dnoutpf.write('\n}\n')
-  upoutpf.write('include '+fileroot+'.sys')
-  dnoutpf.write('include '+fileroot+'.sys')
-
-def produce_inputs_spinless(fileroot):
-  openf = open(fileroot+'.slater','r')
-  outpf = open('orbplot','w')
-  orbs = []
-
-  outpf.write('method { PLOT\n')
-  outpf.write('  resolution 0.1 \n')
-
-  ln = openf.readline().split()[0]
-
-  while ln.split()[0] != '}': 
-    ln = openf.readline()
-    outpf.write(ln)
-
-  while ln != ['#Spin','up','orbitals']: 
-    ln = openf.readline().split()
-      
-  ln = openf.readline().split()
-  while ln != ['#Spin','down','orbitals']:
-    orbs += ln
-    ln = openf.readline().split()
-
-  outpf.write('PLOTORBITALS{ ')
-  for oi in orbs: outpf.write(oi+' ')
-  outpf.write('}')
-  outpf.write('\n}\n')
-  outpf.write('include '+fileroot+'.sys')
+  print('\n'.join(outlines))
 
 if __name__ == "__main__":
-  from sys import argv
-  if len(argv) < 2:
-    raise AssertionError("""
-    Not enough arguements.
-    Usage: getorbs.py qwalk_root [spinless]""")
-  fileroot = argv[1]
-  if len(argv)>2 and argv[2]=="spinless":
-    produce_inputs_spinless(fileroot)
-  else:
-    produce_inputs(fileroot)
+  main()
