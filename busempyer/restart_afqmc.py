12bf0401bc7cda302d8accb675efebc27b72c436
Brian Busemeyer <bbusemeyer@gmail.com>
2020-12-21 12:50:36 -0800

    new file:   restart_afqmc.py

---
+++ busempyer/restart_afqmc.py
@@ -0,0 +1,28 @@
+''' Set up a restart for an AFQMC job.'''
+from busempyer.afqmc_tools import read_afqmc_param, dump_afqmc_param
+
+def main():
+  '''Command-line usage.'''
+  from sys import argv
+
+  prep_restart(argv[-1])
+
+def prep_restart(loc="./"):
+  ''' Save the old afqmc_params and make a new one that restarts the job.'''
+  if loc[-1] != '/': loc += '/'
+  params = read_afqmc_param(loc+"afqmc_param")
+
+  params['initialWalkerFlag'] = "readAllWalkers"
+  params['seed'] = -1
+  params['thermalSize'] = 0
+  params['backGroundInit'] = "readFromFile"
+  params['ETAdjustMaxSize'] = 0
+  params['ETAndBackGroundGrowthEstimateMaxSize'] = 0
+  
+  with open(loc+"past.afqmc_param",'a') as outf:
+    with open(loc+"afqmc_param",'r') as inpf:
+      outf.write(inpf.read())
+
+  dump_afqmc_param(**params)
+
+if __name__=='__main__': main()
